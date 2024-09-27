30fa0e9af8f51cd126730fa686d61562d68b3c3e
Chun-Kai Wang <chunkai1312@gmail.com>
2024-09-27 17:52:04 +0800

    feat(api): setup ngrok ingress

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SessionBuilder } from '@ngrok/ngrok';
 import { AppModule } from './app/app.module';
 
 const port = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   await app.listen(port);
   Logger.log(`🚀 Application is running on port ${port}`);
+
+  if (process.env.NGROK_ENABLED === 'true') {
+    const session = await new SessionBuilder().authtokenFromEnv().connect();
+    const listener = await session.httpEndpoint().listen();
+    Logger.log(`⏩ Ingress established at ${listener.url()}`);
+    listener.forward(`localhost:${port}`);
+  }
 }
 
 bootstrap();
