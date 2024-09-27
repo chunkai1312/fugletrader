fdbc094e18f351e596e4a28134be27500a6d803c
Chun-Kai Wang <chunkai1312@gmail.com>
2024-09-27 17:21:51 +0800

    feat(api): integrate the @fugle/trade package to enable trading operations

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { FugleTradeModule } from '@fugle/trade-nest';
+import { TraderModule } from './trader/trader.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    FugleTradeModule.forRoot({
+      config: {
+        apiUrl: process.env.FUGLE_TRADE_API_URL,
+        apiKey: process.env.FUGLE_TRADE_API_KEY,
+        apiSecret: process.env.FUGLE_TRADE_API_SECRET,
+        aid: process.env.FUGLE_TRADE_AID,
+        password: process.env.FUGLE_TRADE_PASSWORD,
+        certPath: process.env.FUGLE_TRADE_CERT_PATH,
+        certPass: process.env.FUGLE_TRADE_CERT_PASS,
+      },
+    }),
+    TraderModule,
+  ],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.setGlobalPrefix('api');
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application is running on port ${port}`);
 }
 
 bootstrap();
