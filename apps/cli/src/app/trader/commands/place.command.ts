966890a7369af48d9cec7c084b958a51c7296ce8
Chun-Kai Wang <chunkai1312@gmail.com>
2024-09-29 13:05:13 +0800

    feat(cli): adjust place command options and improve the interaction flow

--- apps/cli/src/app/trader/commands/place.command.ts
+++ apps/cli/src/app/trader/commands/place.command.ts
@@ -1,21 +1,18 @@
 import { Command, CommandRunner, InquirerService, Option } from 'nest-commander';
 import { green, red } from 'chalk';
 import { TraderService } from '../trader.service';
+import { PlaceOrderDto } from '../dto/place-order.dto';
+
+type PlaceOrderOptions = PlaceOrderDto & { confirm: boolean };
 
 interface PlaceCommandOptions {
-  stockNo: string;
-  apCode: string;
-  trade: string;
-  bsFlag: string;
-  buySell: string;
-  priceFlag: string;
-  price: number;
-  quantity: number;
-  confirm: number;
+  buy?: boolean;
+  sell?: boolean;
 }
 
 @Command({
   name: 'place',
+  arguments: '[symbol]',
   aliases: ['trade'],
   description: 'place an order',
 })
@@ -28,25 +25,48 @@ export class PlaceCommand extends CommandRunner {
   }
 
   async run(passedParam: string[], options?: PlaceCommandOptions): Promise<void> {
-    const answers = await this.inquirer.prompt('place-order', options);
-    const { confirm, ...order } = answers;
-    if (confirm) {
-      try {
+    try {
+      const [symbol] = passedParam;
+      const { buy, sell } = options;
+
+      if (buy && sell) {
+        throw new TypeError('Only one of the \'buy\' or \'sell\' options can be specified');
+      }
+
+      const stockNo = symbol;
+      const buySell = buy ? 'B' : sell ? 'S' : undefined;
+
+      const answers = await this.inquirer.prompt('place-order', {
+        stockNo,
+        buySell
+      }) as PlaceOrderOptions;
+
+      const { confirm, ...order } = answers;
+
+      if (confirm) {
         await this.traderService.placeOrder(order);
         console.log(`${green('✔')} 已下單`);
-      } catch (err) {
-        console.error(`${red('✖')} ${err.message}`);
-      } finally {
-        console.log();
       }
+    } catch (err) {
+      console.error(`${red('✖')} ${err.message}`);
+    } finally {
+      console.log();
     }
   }
 
   @Option({
-    flags: '-s, --symbol [string]',
-    description: '股票代號',
+    flags: '-b, --buy',
+    description: 'place a buy order',
   })
-  parseSymbol(val: string): string {
-    return val;
+  parseBuy(): boolean {
+    return true;
+  }
+
+  @Option({
+    flags: '-s, --sell',
+    description: 'place a sell order',
+  })
+  parseSell(): boolean {
+    return true;
   }
 }
