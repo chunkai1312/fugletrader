3a055cc84bcab9101c60397ca2f15228577902e3
Chun-Kai Wang <chunkai1312@gmail.com>
2024-09-29 22:48:27 +0800

    feat(cli): allow selection of orders eligible for replacement in the replace command

--- apps/cli/src/app/trader/commands/replace.command.ts
+++ apps/cli/src/app/trader/commands/replace.command.ts
@@ -1,47 +1,68 @@
 import { Command, CommandRunner, InquirerService, Option } from 'nest-commander';
 import { green, red } from 'chalk';
 import { TraderService } from '../trader.service';
+import { ReplaceOrderDto } from '../dto/replace-order.dto';
+
+type ReplaceOrderOptions = ReplaceOrderDto & { id: string, confirm: boolean };
 
 interface ReplaceOrderCommandOptions {
-  id: string;
-  type: string;
-  price: number;
-  quantity: number;
-  confirm: number;
+  price?: number;
+  quantity?: number;
 }
 
 @Command({
   name: 'replace',
+  arguments: '[order-id]',
   description: 'replace a working order',
 })
 export class ReplaceOrderCommand extends CommandRunner {
   constructor(
-    private readonly inquirer: InquirerService,
+    private readonly inquirerService: InquirerService,
     private readonly traderService: TraderService
   ) {
     super();
   }
 
   async run(passedParam: string[], options?: ReplaceOrderCommandOptions): Promise<void> {
-    const answers = await this.inquirer.prompt('replace-order', options);
-    const { confirm, id, ...order } = answers;
-    if (confirm) {
-      try {
+    try {
+      const { price, quantity } = options;
+
+      if (price && quantity) {
+        throw new TypeError('Only one of the \'price\' or \'quantity\' options can be specified');
+      }
+
+      const answers = await this.inquirerService.prompt('replace-order', {
+        id: passedParam[0],
+        price,
+        quantity,
+      }) as ReplaceOrderOptions;
+
+      const { confirm, id, ...order } = answers;
+
+      if (confirm) {
         await this.traderService.replaceOrder(id, order);
         console.log(`${green('✔')} 已改單`);
-      } catch (err) {
-        console.error(`${red('✖')} ${err.message}`);
-      } finally {
-        console.log();
       }
+    } catch (err) {
+      console.error(`${red('✖')} ${err.message}`);
+    } finally {
+      console.log();
     }
   }
 
   @Option({
-    flags: '-i, --id [string]',
-    description: '委託書號',
+    flags: '-p, --price [number]',
+    description: 'set the new price for the order',
   })
-  parseId(val: string): string {
-    return val;
+  parsePrice(val: string): number {
+    return Number(val);
+  }
+
+  @Option({
+    flags: '-q, --quantity [number]',
+    description: 'set the new quantity for the order',
+  })
+  parseQuantity(val: string): number {
+    return Number(val);
   }
 }
--- apps/cli/src/app/trader/questions/replace-order.question.ts
+++ apps/cli/src/app/trader/questions/replace-order.question.ts
@@ -1,10 +1,53 @@
-import { Question, QuestionSet, WhenFor } from 'nest-commander';
+import { Question, QuestionSet, ChoicesFor } from 'nest-commander';
+import { yellow } from 'chalk';
+import { TraderService } from '../trader.service';
 
 @QuestionSet({ name: 'replace-order' })
 export class ReplaceOrderQuestion {
+  constructor(private readonly traderService: TraderService) { }
+
+  @ChoicesFor({ name: 'id' })
+  async getIdChoices() {
+    const orders = await this.traderService.getOrders();
+
+    const choices = orders
+      .filter(order => order.ordNo && order.celable === '1')
+      .map(order => {
+        const id = order.ordNo || order.preOrdNo;
+        const symbol = order.stockNo;
+        const side = this.getOrderSideName(order.buySell);
+        const trade = this.getTradeTypeName(order.trade);
+        const type = this.getOrderTypeName(order.bsFlag);
+
+        const price = (() => {
+          const price = Number(order.odPrice);
+          if (order.apCode === '2') return '收盤價';
+          return (price === 0) ? this.getPriceTypeName(order.priceFlag) : price;
+        })();
+
+        const size = (() => {
+          const sign = order.buySell === 'B' ? '+' : '-';
+          const size = order.orgQtyShare - order.celQtyShare;
+          return `${sign}${size}`;
+        })();
+
+        return {
+          name: `${id}: ${type} ${trade}${side} ${size} (${symbol}) @${price}`,
+          value: id,
+        };
+      });
+
+    if (!choices.length) {
+      console.log(`${yellow('⚠︎')} There are no orders available for replacement`);
+      process.exit(0);
+    }
+
+    return choices;
+  }
+
   @Question({
-    type: 'input',
-    message: '委託書號:',
+    type: 'list',
+    message: '選擇修改委託單:',
     name: 'id',
   })
   parseId(val: string) {
@@ -19,6 +62,7 @@ export class ReplaceOrderQuestion {
       { name: '改價', value: 'price' },
       { name: '改量', value: 'quantity' },
     ],
+    when: (input) => !input.price && !input.quantity,
   })
   parseApCode(val: string) {
     return val;
@@ -46,10 +90,48 @@ export class ReplaceOrderQuestion {
 
   @Question({
     type: 'confirm',
-    message: '確認委託無誤?',
+    message: '確定修改嗎?',
     name: 'confirm',
   })
   parseConfirm(val: string) {
     return val;
+  }
+
+  private getOrderSideName(side: string) {
+    const names = {
+      'B': '買進',
+      'S': '賣出',
+    }
+    return names[side];
+  }
+
+  private getOrderTypeName(bsFlag: string) {
+    const names = {
+      'R': 'ROD',
+      'I': 'IOC',
+      'F': 'FOK',
+    }
+    return names[bsFlag];
+  }
+
+  private getPriceTypeName(priceFlag: string) {
+    const names = {
+      '0': '限價',
+      '1': '平盤價',
+      '2': '跌停價',
+      '3': '漲停價',
+      '4': '市價',
+    }
+    return names[priceFlag];
+  }
+
+  private getTradeTypeName(trade: string) {
+    const names = {
+      '0': '現股',
+      '3': '融資',
+      '4': '融券',
+      'A': '現沖賣',
+    }
+    return names[trade];
   }
 }
